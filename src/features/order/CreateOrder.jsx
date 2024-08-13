31df1a498a6d758da9124c3f8c3a525d6fac4f73
Saidjamolhon Ikramov <saidjamol1@gmail.com>
2024-08-13 16:52:46 +0500

    add some styling

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -7,6 +7,7 @@ import {
   useNavigation,
 } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
+import Button from "../../ui/Button";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -54,13 +55,13 @@ function CreateOrder() {
       <Form method="POST">
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input type="text" name="customer" required className="input"/>
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type="tel" name="phone" required />
+            <input type="tel" name="phone" required className="input"/>
           </div>
           {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
@@ -68,7 +69,9 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input type="text" name="address" required />
+            <input type="text" name="address" required
+              className="input"
+            />
           </div>
         </div>
 
@@ -77,17 +80,19 @@ function CreateOrder() {
             type="checkbox"
             name="priority"
             id="priority"
+            className="h-6 w-6 accent-yellow-400
+            focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
-          />
+          /> 
           <label htmlFor="priority">Want to yo give your order priority?</label>
         </div>
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>
+          <Button>
             {isSubmitting ? "Placing order" : "Order now"}
-          </button>
+            </Button>
         </div>
       </Form>
     </div>
