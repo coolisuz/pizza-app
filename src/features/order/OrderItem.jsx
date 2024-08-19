c2c05d72078476027e28597e7c88355f479e5464
Saidjamolhon Ikramov <saidjamol1@gmail.com>
2024-08-20 00:00:46 +0500

    touch of style

--- src/features/order/OrderItem.jsx
+++ src/features/order/OrderItem.jsx
@@ -1,15 +1,16 @@
-import { formatCurrency } from "../../utils/helpers";
+// import { formatCurrency } from "../../utils/helpers";
+import { formatCurrency } from "../../utilities/helpers";
 
 function OrderItem({ item, isLoadingIngredients, ingredients }) {
   const { quantity, name, totalPrice } = item;
 
   return (
     <li>
-      <div>
+      <div className="flex items-center justify-between px-2 space-y-2 flex-wrap">
         <p>
-          <span>{quantity}&times;</span> {name}
+          <span className="font-bold">{quantity}&times;</span> {name}
         </p>
-        <p>{formatCurrency(totalPrice)}</p>
+        <p className="font-bold">{formatCurrency(totalPrice)}</p>
       </div>
     </li>
   );
